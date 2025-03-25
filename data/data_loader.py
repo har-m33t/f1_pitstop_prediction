e5b0e3f5cdd17913670077fab8eb6dd576a22e0f
Harmeet Singh <harmees2@uci.edu>
2025-03-24 19:34:03 -0700

    Implemented more features: - Laps Since Pitting - Tire Degradation Rate - Tire Efficiency Score

--- data/data_loader.py
+++ data/data_loader.py
@@ -23,7 +23,7 @@ class TireData:
         print('SUMMARY STATISTICS')
         print(self.df.describe())
     
-    #FEATURE ENGINEERING
+    # FEATURE ENGINEERING
     def calc_laptime_avg(self):
         self.avg_lap_time = self.df.groupby('LapNumber')['LapTime'].mean().reset_index()
         self.avg_lap_time.rename(columns={'LapTime': 'AvgLapTime'}, inplace = True)
@@ -46,11 +46,31 @@ class TireData:
 
         return pd.Series(cumulative_wear, index=group.index)
     
+    def add_laps_since_pitting(self, group):
+        laps_since_pitting = []
+        laps = 0 
+        for _, row in group.iterrows():
+            if row['PitStop']:
+                laps = 0
+            else:
+                laps += 1
+            laps_since_pitting.append(laps)
+        return pd.Series(laps_since_pitting, index = group.index)
+
+    def calc_degrade_rate(self):
+        self.df['DegradationRate'] = self.df['TireWear'] / self.df['LapsSincePit']
+
+    def calc_tire_efficiency(self):
+        self.df['TireEfficiency'] = self.df['LapTime'] / (1 + self.df['TireWear'])
+    
     def feature_engineer(self):
         self.calc_laptime_avg()
         self.calc_laptime_delta()
         self.add_pitstops()
         self.df['CumulativeTireWear'] = self.df.groupby('DriverID', group_keys=False).apply(self.calc_cumwear)
+        self.df['LapsSincePit'] = self.df.groupby('DriverID', group_keys = False).apply(self.add_laps_since_pitting)
+        self.calc_degrade_rate()
+        self.calc_tire_efficiency()
 
 if __name__ == '__main__':
     file_path = 'f1_tire_strategy_simulation_v2.csv'
