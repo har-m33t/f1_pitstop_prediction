8baae279d20027ed57e1193a2878a408822cd59d
Harmeet Singh <harmees2@uci.edu>
2025-03-24 19:01:00 -0700

    Implemented Feature Engineering by Calculating Cumulative Tire Wear

--- data/data_loader.py
+++ data/data_loader.py
@@ -24,21 +24,38 @@ class TireData:
         print(self.df.describe())
     
     #FEATURE ENGINEERING
-    def calc_laptime_delta(self):
+    def calc_laptime_avg(self):
         self.avg_lap_time = self.df.groupby('LapNumber')['LapTime'].mean().reset_index()
         self.avg_lap_time.rename(columns={'LapTime': 'AvgLapTime'}, inplace = True)
         self.df = pd.merge(self.df, self.avg_lap_time, on = 'LapNumber')
+
+    def calc_laptime_delta(self):
         self.df['LapTimeDelta'] = self.df['LapTime'] - self.df['AvgLapTime']
     
-    def calc_cum_tirewear(self):
-        self.df['PitInstance'] = self.df['TotalPitStops'].ne(self.df['TotalPitStops'].shift()).cumsum()
-        self.df['cumsum'] = self.df.groupby('value_change')['amount'].cumsum()
+    def add_pitstops(self):
+        self.df['PitStop'] = self.df['TireWear'] == 0
+    
+    def calc_cumwear(self, group):
+        cumulative_wear = []
+        current_wear = 0
+        for _, row in group.iterrows():
+            if row['PitStop']:  # Reset the wear after a pit stop
+                current_wear = 0
+            current_wear += row['TireWear']
+            cumulative_wear.append(current_wear)
 
+        return pd.Series(cumulative_wear, index=group.index)
+    
+    def feature_engineer(self):
+        self.calc_laptime_avg()
+        self.calc_laptime_delta()
+        self.add_pitstops()
+        self.df['CumulativeTireWear'] = self.df.groupby('DriverID', group_keys=False).apply(self.calc_cumwear)
 
 if __name__ == '__main__':
     file_path = 'f1_tire_strategy_simulation_v2.csv'
     data = TireData(file_path)
     df = data.create_df()
-    data.calc_laptime_delta()
+    data.feature_engineer()
     data.check_df()
     
